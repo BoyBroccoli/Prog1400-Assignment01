46107cdf24faa6efbad60fb64e0f078fa821da63
Leona Esther <leonaesther@pm.me>
2022-10-03 22:46:26 -0300

    Classes created

---
+++ inc/Player.java
@@ -0,0 +1,54 @@
+package inc;
+
+class Player {
+    
+    // INSTANCE VARIABLES
+    private String playerName;
+    private double playerGoals;
+    private double playerAssists;
+    private double playerTotal;
+
+    // METHODS 
+
+    public void outputPlayerDetails(){
+
+    }
+
+    // GETTERS
+
+    public double getPlayerGoals(){
+        return this.playerGoals;
+    }
+
+    public double getPlayerAssists(){
+        return this.playerAssists;
+    }
+
+    public double getPlayerTotal(){
+        return this.playerTotal;
+    }
+
+    public String getPlayerName(){
+        return this.playerName;
+    }
+
+
+
+    // SETTERS
+
+    public String setPlayerName(String name){
+        return this.playerName = name;
+    }
+
+    public double setPlayerGoals(double goals){
+        return this.playerGoals = goals;
+    }
+
+    public double setPlayerAssists(double assists){
+        return this.playerAssists = assists;
+    }
+
+    public double setPlayerTotal(){
+        return this.playerTotal = this.playerGoals + this.playerAssists;
+    }
+}
---
+++ inc/Team.java
@@ -0,0 +1,70 @@
+package inc;
+
+class Team {
+
+    // INSTANCE VARIABLES
+    private String teamName;
+    private double teamGoals;
+    private double teamAssists;
+    private double teamTotal;
+    double teamBudget;
+    String teamStars;
+
+    // METHODS 
+
+    public void outputTeamDetails(){
+
+    }
+
+    // GETTERS
+
+    public double getTeamGoals(){
+        return this.teamGoals;
+    }
+
+    public double getTeamAssists(){
+        return this.teamAssists;
+    }
+
+    public double getTeamTotal(){
+        return this.teamTotal;
+    }
+
+    public String getTeamName(){
+        return this.teamName;
+    }
+
+    public double getTeamBudget(){
+        return this.teamBudget;
+    }
+
+    public String getTeamStars(){
+        if (this.teamTotal > 20){
+            this.teamStars = "***";
+        } else if (this.teamTotal >= 10) {
+            this.teamStars = "**";
+        } else {
+            this.teamStars =" ";
+        }
+        return this.teamStars;
+    }
+
+    // SETTERS
+
+    public String setTeamName(String name){
+        return this.teamName = name;
+    }
+
+    public double setTeamGoals(double goals){
+        return this.teamGoals = goals;
+    }
+
+    public double setTeamAssists(double assists){
+        return this.teamAssists = assists;
+    }
+
+    public double setTeamTotal(){
+        return this.teamTotal = this.teamGoals + this.teamAssists;
+    }
+
+}
